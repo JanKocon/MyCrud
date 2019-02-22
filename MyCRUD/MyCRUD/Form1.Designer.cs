ede81ff12de00e92ef4a507ead698d612065f1ab
Jan <jankocon7@gmail.com>
2019-02-22 12:45:57 +0100

    Added project

---
+++ MyCRUD/MyCRUD/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace MyCRUD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Name_TextBox = new System.Windows.Forms.TextBox();
+            this.Number_TextBox = new System.Windows.Forms.TextBox();
+            this.Address_TextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Update_Button = new System.Windows.Forms.Button();
+            this.Delete_Button = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.ID_Label = new System.Windows.Forms.Label();
+            this.Add_Button = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(371, 72);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(592, 366);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // Name_TextBox
+            // 
+            this.Name_TextBox.Location = new System.Drawing.Point(92, 91);
+            this.Name_TextBox.Name = "Name_TextBox";
+            this.Name_TextBox.Size = new System.Drawing.Size(201, 22);
+            this.Name_TextBox.TabIndex = 1;
+            // 
+            // Number_TextBox
+            // 
+            this.Number_TextBox.Location = new System.Drawing.Point(92, 150);
+            this.Number_TextBox.Name = "Number_TextBox";
+            this.Number_TextBox.Size = new System.Drawing.Size(201, 22);
+            this.Number_TextBox.TabIndex = 2;
+            // 
+            // Address_TextBox
+            // 
+            this.Address_TextBox.Location = new System.Drawing.Point(92, 204);
+            this.Address_TextBox.Multiline = true;
+            this.Address_TextBox.Name = "Address_TextBox";
+            this.Address_TextBox.Size = new System.Drawing.Size(201, 76);
+            this.Address_TextBox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(28, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(28, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Number";
+            // 
+            // Update_Button
+            // 
+            this.Update_Button.BackColor = System.Drawing.Color.Lime;
+            this.Update_Button.ForeColor = System.Drawing.Color.White;
+            this.Update_Button.Location = new System.Drawing.Point(31, 335);
+            this.Update_Button.Name = "Update_Button";
+            this.Update_Button.Size = new System.Drawing.Size(120, 44);
+            this.Update_Button.TabIndex = 6;
+            this.Update_Button.Text = "Update";
+            this.Update_Button.UseVisualStyleBackColor = false;
+            this.Update_Button.Click += new System.EventHandler(this.Update_OnClick);
+            // 
+            // Delete_Button
+            // 
+            this.Delete_Button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.Delete_Button.ForeColor = System.Drawing.Color.White;
+            this.Delete_Button.Location = new System.Drawing.Point(173, 335);
+            this.Delete_Button.Name = "Delete_Button";
+            this.Delete_Button.Size = new System.Drawing.Size(120, 44);
+            this.Delete_Button.TabIndex = 7;
+            this.Delete_Button.Text = "Delete";
+            this.Delete_Button.UseVisualStyleBackColor = false;
+            this.Delete_Button.Click += new System.EventHandler(this.Delete_OnClick);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(696, 15);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(83, 41);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Search";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(371, 24);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(304, 22);
+            this.textBox4.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(28, 207);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 17);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Address";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(31, 47);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(21, 17);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "ID";
+            // 
+            // ID_Label
+            // 
+            this.ID_Label.AutoSize = true;
+            this.ID_Label.ForeColor = System.Drawing.Color.White;
+            this.ID_Label.Location = new System.Drawing.Point(89, 47);
+            this.ID_Label.Name = "ID_Label";
+            this.ID_Label.Size = new System.Drawing.Size(0, 17);
+            this.ID_Label.TabIndex = 13;
+            // 
+            // Add_Button
+            // 
+            this.Add_Button.BackColor = System.Drawing.Color.GhostWhite;
+            this.Add_Button.Location = new System.Drawing.Point(31, 385);
+            this.Add_Button.Name = "Add_Button";
+            this.Add_Button.Size = new System.Drawing.Size(262, 42);
+            this.Add_Button.TabIndex = 14;
+            this.Add_Button.Text = "Add";
+            this.Add_Button.UseVisualStyleBackColor = false;
+            this.Add_Button.Click += new System.EventHandler(this.Add_Button_OnClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.ClientSize = new System.Drawing.Size(1009, 450);
+            this.Controls.Add(this.Add_Button);
+            this.Controls.Add(this.ID_Label);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.Delete_Button);
+            this.Controls.Add(this.Update_Button);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Address_TextBox);
+            this.Controls.Add(this.Number_TextBox);
+            this.Controls.Add(this.Name_TextBox);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox Name_TextBox;
+        private System.Windows.Forms.TextBox Number_TextBox;
+        private System.Windows.Forms.TextBox Address_TextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Update_Button;
+        private System.Windows.Forms.Button Delete_Button;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label ID_Label;
+        private System.Windows.Forms.Button Add_Button;
+    }
+}
+
---
+++ MyCRUD/MyCRUD/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyCRUD
+{
+    public partial class Form1 : Form
+    {
+        SQLiteConnection sqlite;
+
+        public void InitSQLiteConnection()
+        {
+            string path = Environment.CurrentDirectory + "\\JohhnyDataBase.db";
+            sqlite = new SQLiteConnection("Data source=" + path);
+        }
+        public DataTable selectQuery(string query)
+        {
+            SQLiteDataAdapter adapter;
+            DataTable dt = new DataTable();
+
+            try
+            {
+                SQLiteCommand cmd;
+                sqlite.Open();  
+                cmd = sqlite.CreateCommand();
+                cmd.CommandText = query;
+                adapter = new SQLiteDataAdapter(cmd);
+                adapter.Fill(dt); 
+            }
+            catch (SQLiteException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if(sqlite != null)
+                    sqlite.Close();
+            }
+            return dt;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            InitSQLiteConnection();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            UpdateDataGrid();
+        }
+        private void UpdateDataGrid()
+        {
+            DataTable dt = selectQuery("select * from Clients");
+            dataGridView1.DataSource = dt;
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            ID_Label.Text = dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString();
+            Name_TextBox.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+            Number_TextBox.Text = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+            Address_TextBox.Text = dataGridView1.Rows[e.RowIndex].Cells[3].Value.ToString();
+            
+        }
+
+        private void Delete_OnClick(object sender, EventArgs e)
+        {
+            string query = $"delete from Clients where ID={ID_Label.Text}";
+            selectQuery(query);
+            UpdateDataGrid();
+
+        }
+
+        private void Update_OnClick(object sender, EventArgs e)
+        {
+            string number = Number_TextBox.Text.Trim();
+            if (number.All(char.IsDigit))
+            {
+                string query = $"update Clients set Name='{Name_TextBox.Text}'," +
+                               $" Number={Number_TextBox.Text}," +
+                               $" Address='{Address_TextBox.Text}' " +
+                               $"where ID={ID_Label.Text}";
+                selectQuery(query);
+                UpdateDataGrid();
+            }
+            else
+                MessageBox.Show("Number has to be numerical");
+
+        }
+
+        private void Add_Button_OnClick(object sender, EventArgs e)
+        {
+            string query = "insert into Clients (Name, Number, Address)" +
+                           $" values (('{Name_TextBox.Text}'),('{Number_TextBox.Text}'),('{Address_TextBox.Text}'))";
+            selectQuery(query);
+            UpdateDataGrid();
+        }
+    }
+}
